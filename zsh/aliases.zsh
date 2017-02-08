7a47e6fff32df74b9924c8cdc13a6ab100806fe5
Matt Sorenson <matt@mattsorenson.com>
2017-02-08 15:05:03 -0800

    Removed some unused aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,18 +4,12 @@ alias vi=vim
 
 alias strip-color-codes="perl -pe 's/\e\[?.*?[\@-~]//g'"
 
-alias sshc='ssh -F /dev/null'
-
 ws() {
     if [ ${#} -gt 1 ]; then
         cd "$WORKSPACE_ROOT_DIR/$1/src/$2";
     else
         cd "$WORKSPACE_ROOT_DIR/$1"
     fi
-}
-
-mcd() {
-    mkdir -p "$@" && cd "$@"
 }
 
 clang-format-ri() {
