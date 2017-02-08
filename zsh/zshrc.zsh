e061e762f339d3737b1d7235874be221f7a481ed
Matt Sorenson <matt@mattsorenson.com>
2017-02-08 15:11:29 -0800

    Added some underscores to make 'setopt's more clear

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -25,18 +25,18 @@ HIST_STAMPS="yyyy-mm-dd"
 HISTFILE="$HOME/.dotfiles/tmp/history"
 HISTSIZE=101000
 SAVEHIST=100000
-setopt appendhistory
+setopt append_history
 setopt hist_expire_dups_first
 setopt hist_reduce_blanks
 setopt hist_ignore_dups
 setopt hist_find_no_dups
-setopt histverify
+setopt hist_verify
 
-setopt interactivecomments # treat comments as comments in interactive shell
-setopt autopushd
-setopt autocd
-setopt completeinword
-setopt nobeep
+setopt interactive_comments # treat comments as comments in interactive shell
+setopt auto_pushd
+setopt auto_cd
+setopt complete_inword
+setopt no_beep
 
 prompt ender
 
