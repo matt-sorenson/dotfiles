d5abae3176fd137abe4d7bd7c75a026d38a7b9f8
Matt Sorenson <matt@mattsorenson.com>
2017-02-13 15:26:30 -0800

    Updated zsh setops

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -25,17 +25,24 @@ HIST_STAMPS="yyyy-mm-dd"
 HISTFILE="$HOME/.dotfiles/tmp/history"
 HISTSIZE=101000
 SAVEHIST=100000
-setopt append_history
 setopt hist_expire_dups_first
 setopt hist_reduce_blanks
 setopt hist_ignore_dups
 setopt hist_find_no_dups
 setopt hist_verify
+setopt share_history
+setopt no_append_history
 
-setopt interactive_comments # treat comments as comments in interactive shell
+# Directory stack options
 setopt auto_pushd
 setopt auto_cd
+setopt pushd_to_home
+setopt no_pushd_silent
+
+# Random settings
 setopt complete_inword
+setopt extended_glob
+setopt interactive_comments # treat comments as comments in interactive shell
 setopt no_beep
 
 prompt ender
