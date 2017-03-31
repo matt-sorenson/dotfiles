6274ec98f841c213237adfc3cfd7085a7ef6ceab
Matt Sorenson <matt@mattsorenson.com>
2017-03-30 18:11:26 -0700

    update default layout

--- hammerspoon/default_layout.lua
+++ hammerspoon/default_layout.lua
@@ -10,9 +10,9 @@ layout.add_app('default', 'Google Chrome', nil, 'primary',  2/3, 0, 1/3, 1)
 layout.add_app('default', 'Firefox',       nil, 'primary',  2/3, 0, 1/3, 1)
 
 if sys.is_work_computer() then
-    layout.add_app('default', 'iTerm',    nil,        'secondary',    0,   0, 9/10, 1/2)
+    layout.add_app('default', 'iTerm', nil, 'secondary', 0, 0, 9/10, 1/2)
 else
-    layout.add_app('default', 'iTerm' , nil, 'secondary',   0,   0,   1, 1/3)
+    layout.add_app('default', 'iTerm', nil, 'secondary', 0, 0,    1, 1/2)
 end
 
 local function add_communications_layouts(layout_name)
@@ -22,17 +22,24 @@ local function add_communications_layouts(layout_name)
         layout.add_app(layout_name, 'Adium',    'Contacts', 'secondary',  2/3, 1/2,  1/3, 1/2)
         layout.add_app(layout_name, 'Messages', nil,        'secondary',  1/5, 4/7,  3/5, 3/7)
     else
-        layout.add_app(layout_name, 'Slack',  nil, 'secondary', 1/2, 2/3, 1/2, 1/3)
+        layout.add_app(layout_name, 'Slack',    nil, 'secondary', 0.5 + 0/5, 1/2, (3/5)/2, 1/3)
+        layout.add_app(layout_name, 'Messages', nil, 'secondary', 0.5 + 2/5, 1/2, (3/5)/2, 3/7)
     end
 end
 
 local function add_media_layouts(layout_name)
-    layout.add_app(layout_name, nil,       'Youtube',      'secondary', 1/8, 1/2, 6/8, 1/3)
-    layout.add_app(layout_name, nil,       'Amazon Music', 'secondary', 1/8, 1/2, 6/8, 1/3)
-    layout.add_app(layout_name, 'Spotify', nil,            'secondary', 1/8, 1/2, 6/8, 1/3)
-    layout.add_app(layout_name, 'iTunes',  nil,            'secondary', 1/8, 1/2, 6/8, 1/3)
-    layout.add_app(layout_name, nil,       'reddit',       'secondary', 1/8, 8/14, 6/8, 5.5/14)
-    layout.add_app(layout_name, nil,       'Hacker News',  'secondary', 1/8, 8/14, 6/8, 5.5/14)
+    if sys.is_work_computer() then
+        layout.add_app(layout_name, nil,       'reddit',       'secondary', 1/8, 8/14, 6/8, 5.5/14)
+        layout.add_app(layout_name, nil,       'Hacker News',  'secondary', 1/8, 8/14, 6/8, 5.5/14)
+
+        layout.add_app(layout_name, nil,       'Youtube',      'secondary', 1/8, 1/2, 6/8, 1/3)
+        layout.add_app(layout_name, nil,       'Amazon Music', 'secondary', 1/8, 1/2, 6/8, 1/3)
+        layout.add_app(layout_name, 'Spotify', nil,            'secondary', 1/8, 1/2, 6/8, 1/3)
+        layout.add_app(layout_name, 'iTunes',  nil,            'secondary', 1/8, 1/2, 6/8, 1/3)
+    else
+        layout.add_app(layout_name, 'Spotify', nil,            'secondary',   0, 1/2, 1/2, 1/2)
+        layout.add_app(layout_name, 'iTunes',  nil,            'secondary',   0, 1/2, 1/2, 1/2)
+    end
 end
 
 add_communications_layouts('default')
