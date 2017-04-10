2e46c2e4bdcb378600d1a653bc802969a6ef006e
Matt Sorenson <matt@mattsorenson.com>
2017-04-10 16:09:44 -0700

    simplified music player code

--- hammerspoon/ms/music.lua
+++ hammerspoon/ms/music.lua
@@ -1,18 +1,31 @@
+local function app_js(app, cmd)
+    local script = "var app = Application('" .. app .. "'); " .. cmd
+    local ok, result = hs.osascript.javascript(script)
+
+    return result, ok
+end
+
 local players = {
     itunes = {
         name = 'iTunes',
         bundle_id = 'com.apple.iTunes',
-        player = hs.itunes
+        fn = function(cmd) return  app_js('iTunes', cmd) end
+
+        shuffle = function() return app_js('iTunes', 'app.shuffleEnabled = !app.shuffleEnabled()') end
+        is_shuffled = function() return app_js('iTunes', 'app.shuffleEnabled()') end
     },
 
     spotify = {
         name = 'Spotify',
         bundle_id = 'com.spotify.client',
-        player = hs.spotify
+        fn = function(cmd) app_js('Spotify', cmd) end,
+
+        shuffle = function() return app_js('Spotify', 'app.shuffling = !app.shuffling()') end,
+        is_shuffled = function() return app_js('Spotify', 'spotify', 'app.shuffling()') end
     }
 }
 
-local function current_player_table()
+local function current_player()
     if hs.itunes.isPlaying() then
         return players.itunes
     end
@@ -20,35 +33,28 @@ local function current_player_table()
     return players.spotify
 end
 
-local function current_player()
-    return current_player_table().player
-end
-
-local function current_player_bundleID()
-    return current_player_table().bundle_id
-end
-
-local function current_player_name()
-    return current_player_table().name
-end
-
-local metatable = {
-    __index = function(self, key)
-        print(key)
-        return current_player()[key]
-    end
-}
-
 local out = {
-    current_player_bundleID = current_player_bundleID,
-    current_player_name = current_player_name,
+    current_player_bundleID = function() return current_players().bundle_id end,
+    current_player_name = function() return current_players().name end,
     fn = function(key, ...)
         local args = {...}
         return function()
-            current_player()[key](table.unpack(args))
+            local player = current_player()
+            if player[key] then
+                player[key](table.unpack(args))
+            else
+                player.fn(key)
+            end
         end
     end
 }
+
+local metatable = {
+    __index = function(self, key, ...)
+        current_player().fn(key, ...)
+    end
+}
+
 setmetatable(out, metatable);
 
 return out
