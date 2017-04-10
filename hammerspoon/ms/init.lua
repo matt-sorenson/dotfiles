d4ef975d5ca0adc12dc433a321513bee9b9f5087
Matt Sorenson <matt@mattsorenson.com>
2017-04-10 16:07:25 -0700

    Cleaned up some of the key bindings and descriptions

--- hammerspoon/ms/init.lua
+++ hammerspoon/ms/init.lua
@@ -20,49 +20,45 @@ hs.hotkey.bind('alt', 'space', sys.select_app_fn('iTerm', {toggle = true, new_wi
 
 local global_modal = bind.modal_new(bind.default_modal(), {'ctrl', 'cmd'}, 'B')
 
-local function init_power_modal(parent)
-    local power_modal  = bind.modal_new(parent, {}, 'E', 'Power')
-
-    power_modal:bind({}, 'S', 'Screen Saver', hs.caffeinate.startScreensaver)
-    for i = 1,6 do
-        power_modal:bind({}, tostring(i), 'Caffeine on ' .. i .. '0 Minutes', function() caffeine.timed_on_m(i * 10) end)
-    end
-end
-
 local window_modal = bind.modal_new(global_modal, {}, 'W', 'Window')
 local finder_modal = bind.modal_new(global_modal, {}, 'F', 'Finder')
 local music_modal  = bind.modal_new(global_modal, {}, 'S', 'Music')
+local power_modal  = bind.modal_new(global_modal, {}, 'E', 'Power')
 
-window_modal:bind({'shift'}, 'W', '↑ 33%', bind.set_window_rect({   0,   0,   1, 1/3 }))
-window_modal:bind({'shift'}, 'A', '← 33%', bind.set_window_rect({   0,   0, 1/3,   1 }))
-window_modal:bind({'shift'}, 'S', '↓ 33%', bind.set_window_rect({   0, 2/3,   1, 1/3 }))
-window_modal:bind({'shift'}, 'D', '→ 33%', bind.set_window_rect({ 2/3,   0, 1/3,   1 }))
-window_modal:bind({},        'W', '↑ 50%', bind.set_window_rect({ 0.0, 0.0, 1.0, 0.5 }))
-window_modal:bind({},        'A', '← 50%', bind.set_window_rect({ 0.0, 0.0, 0.5, 1.0 }))
-window_modal:bind({},        'S', '↓ 50%', bind.set_window_rect({ 0.0, 0.5, 1.0, 0.5 }))
-window_modal:bind({},        'D', '→ 50%', bind.set_window_rect({ 0.5, 0.0, 0.5, 1.0 }))
+window_modal:bind({'shift'}, 'W', '33% ↑', bind.set_window_rect({   0,   0,   1, 1/3 }))
+window_modal:bind({'shift'}, 'A', '33% ←', bind.set_window_rect({   0,   0, 1/3,   1 }))
+window_modal:bind({'shift'}, 'S', '33% ↓', bind.set_window_rect({   0, 2/3,   1, 1/3 }))
+window_modal:bind({'shift'}, 'D', '33% →', bind.set_window_rect({ 2/3,   0, 1/3,   1 }))
+window_modal:bind({},        'W', '50% ↑', bind.set_window_rect({ 0.0, 0.0, 1.0, 0.5 }))
+window_modal:bind({},        'A', '50% ←', bind.set_window_rect({ 0.0, 0.0, 0.5, 1.0 }))
+window_modal:bind({},        'S', '50% ↓', bind.set_window_rect({ 0.0, 0.5, 1.0, 0.5 }))
+window_modal:bind({},        'D', '50% →', bind.set_window_rect({ 0.5, 0.0, 0.5, 1.0 }))
 
 window_modal:add_help_seperator()
+window_modal:bind({}, 'Q', "Quiet current window",        bind.set_window_rect({1/8, 8/14, 6/8, 5.5/14}))
 window_modal:bind({}, 'F', 'Maximize',                    bind.set_window_rect({ 0, 0, 1, 1 }))
 window_modal:bind({}, 'G', 'Grid' ,                       hs.grid.show)
-window_modal:bind({}, 'R', 'Apply Layout to Window',      layout.apply_current_window)
+window_modal:bind({}, 'R', 'Apply layout to window',      layout.apply_current_window)
 window_modal:bind({}, 'T', 'Apply Default Layout',        function() layout.apply_layout("Default") end)
 window_modal:bind({}, 'E', 'Apply Media Layout',          function() layout.apply_layout("Media") end)
 window_modal:bind({}, 'C', 'Apply Communications Layout', function() layout.apply_layout("Communications") end)
 
-finder_modal:bind({}, 'G', 'Home', sys.select_app_fn('Finder', {window = sys.who_am_i(), new_window = sys.open_finder_fn() }))
-finder_modal:bind({}, 'W', 'Workspace', sys.select_app_fn('Finder', { window = 'ws', new_window = sys.open_finder_fn(os.getenv('HOME') .. '/ws') }))
-finder_modal:bind({}, 'R', 'Remote Home', sys.select_app_fn('Finder', {window = REMOTE_SHARE_FOLDER, new_window = sys.open_finder_fn('/Volumes/' .. REMOTE_SHARE_FOLDER) }))
+finder_modal:bind({}, 'G', 'Home',        sys.select_app_fn('Finder', { window = sys.who_am_i(), new_window = sys.open_finder_fn('~/') }))
+finder_modal:bind({}, 'W', 'Workspace',   sys.select_app_fn('Finder', { window = 'ws', new_window = sys.open_finder_fn('~/ws') }))
+finder_modal:bind({}, 'R', 'Remote Home', sys.select_app_fn('Finder', { window = REMOTE_SHARE_FOLDER, new_window = sys.open_finder_fn('/Volumes/' .. REMOTE_SHARE_FOLDER) }))
 
-music_modal:bind({}, 'S', hs.utf8.codepointToUTF8('U+23EF'), music.fn('playpause'))
-music_modal:bind({}, 'A', hs.utf8.codepointToUTF8('U+23EE'), music.fn('previous'), { shiftable = true })
-music_modal:bind({}, 'D', hs.utf8.codepointToUTF8('U+23ED'), music.fn('next'),     { shiftable = true })
-music_modal:bind({}, 'C', 'Select Player',                   function() sys.select_app(music.current_player_bundleID()) end)
+music_modal:bind({}, 'S', 'Play/Pause', music.fn('playpause'))
+music_modal:bind({}, 'A', 'Previous', music.fn('previous'), { shiftable = true })
+music_modal:bind({}, 'D', 'Next', music.fn('next'),         { shiftable = true })
+music_modal:bind({}, 'C', 'Select player', function() sys.select_app(music.current_player_bundleID()) end)
 music_modal:add_help_seperator()
-music_modal:bind({}, 'W', 'Raise Volume 1%', audio.update_output_volume_fn( 1), { shiftable = true })
-music_modal:bind({}, 'X', 'Lower Volume 1%', audio.update_output_volume_fn(-1), { shiftable = true })
+music_modal:bind({}, 'W', 'Raise volume', audio.update_output_volume_fn( 1), { shiftable = true })
+music_modal:bind({}, 'X', 'Lower volume', audio.update_output_volume_fn(-1), { shiftable = true })
 
-init_power_modal(global_modal)
+power_modal:bind({}, 'S', 'Screen Saver', hs.caffeinate.startScreensaver)
+for i = 1,6 do
+    power_modal:bind({}, tostring(i), 'Caffeine on ' .. i .. '0 Minutes', function() caffeine.timed_on_m(i * 10) end)
+end
 
 if REMOTE_SHARE_HOST and REMOTE_SHARE_FOLDER then
     sys.mount_smb(REMOTE_SHARE_HOST, REMOTE_SHARE_FOLDER)
