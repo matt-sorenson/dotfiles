9dd10704396b483087d51e485ef523f7a35b8335
Matt Sorenson <matt@mattsorenson.com>
2017-04-26 13:49:45 -0700

    clean up layout definitions by moving layout scores and screen retreival logic into layout.lua

--- hammerspoon/layouts/work-laptop.lua
+++ hammerspoon/layouts/work-laptop.lua
@@ -1,7 +1,7 @@
-local sys    = require 'ms.sys'
-
-local layout = {
+return {
     { -- Primary Screen
+        screen = 'Color LCD',
+
         { app = {'Sublime', 'Quiver', 'IntelliJ', 'iTerm'},  rect = { 0,  0, 4/5, 1} },
         { app = {'Chrome', 'Firefox'},                       rect = {1/5, 0, 4/5, 1} },
 
@@ -11,23 +11,4 @@ local layout = {
         { app = 'Adium',                      rect = {    0, 1/10, 1/3, 9/10 }, layouts = 'communications' },
         { app = 'Adium', window = 'Contacts', rect = {  2/3, 1/10, 1/3, 9/10 }, layouts = 'communications' },
     }
-}
-
-local function score(self)
-    if sys.is_work_computer() and hs.screen('Color LCD') then
-        return 2
-    end
-
-    return -1
-end
-
-local function screens()
-    return { hs.screen('Color LCD') }
-end
-
-return {
-    score = score,
-    layout = layout,
-
-    screens = screens
 }
