8a2c7ac4661523a0c80b133a9ec05656c6b96962
Matt Sorenson <matt@mattsorenson.com>
2017-05-05 12:22:11 -0700

    update work-ultrawide-vertical to make iterm larger

--- hammerspoon/layouts/work-ultrawide-vertical.lua
+++ hammerspoon/layouts/work-ultrawide-vertical.lua
@@ -10,15 +10,15 @@ return {
     { -- Secondary Screen
         screen = '1440x2560',
 
-        { app = 'iTerm', rect = { 0, 0, 9/10, 1/2} },
+        { app = 'iTerm', rect = { 0, 0, 9/10, 2/3} },
 
         { window = {'reddit', 'Hacker News'}, rect = { 1/8, 8/14, 6/8, 11/28 }, layouts = 'media' },
         { window = 'Youtube',                 rect = { 1/8,  1/2, 6/8,   1/3 }, layouts = 'media' },
 
-        { app = 'Slack',                      rect = { 1/10,   0, 9/10, 1/2 }, layouts = 'communications' },
-        { app = 'Adium',                      rect = {    0, 1/2,  1/3, 1/2 }, layouts = 'communications' },
-        { app = 'Adium', window = 'Contacts', rect = {  2/3, 1/2,  1/3, 1/2 }, layouts = 'communications' },
-        { app = 'Messages',                   rect = {  1/5, 4/7,  3/5, 3/7 }, layouts = 'communications' },
+        { app = 'Slack',                      rect = { 1/10,   0, 9/10, 2/3 }, layouts = 'communications' },
+        { app = 'Adium',                      rect = {    0, 2/3,  1/3, 1/3 }, layouts = 'communications' },
+        { app = 'Adium', window = 'Contacts', rect = {  2/3, 2/3,  1/3, 1/3 }, layouts = 'communications' },
+        { app = 'Messages',                   rect = {  1/5, 4/6,  3/5, 3/7 }, layouts = 'communications' },
     },
 
     is_work_computer = true,
