f53ad83775ebca8809b5aa510507f5cbe73c77c3
Matt Sorenson <matt@mattsorenson.com>
2017-05-17 12:00:19 -0700

    add intellij to home layout

--- hammerspoon/layouts/home-4k-laptop.lua
+++ hammerspoon/layouts/home-4k-laptop.lua
@@ -2,8 +2,8 @@ return {
     { -- Primary Screen
         screen = 'DELL P2715Q',
 
-        { app = {'Sublime', 'Quiver'}, rect = {   0, 0, 1/2, 1 } },
-        { app = {'Chrome', 'Firefox'}, rect = { 1/2, 0, 1/2, 1 } },
+        { app = {'Sublime', 'Quiver', 'IntelliJ'}, rect = {   0, 0, 1/2, 1 } },
+        { app = {'Chrome', 'Firefox'},             rect = { 1/2, 0, 1/2, 1 } },
     },
     { -- Secondary Screen
         screen = 'Color LCD',
@@ -12,6 +12,8 @@ return {
 
         { app = 'Slack',    rect = {   0, 1/2, 1/2, 1/2 }, layouts = 'communications' },
         { app = 'Messages', rect = { 1/2, 1/2, 1/2, 1/2 }, layouts = 'communications' },
+        { app = 'Adium',    rect = {   0, 1/2, 1/3, 1/2 }, layouts = 'communications' },
+        { app = 'Chime',    rect = { 1/2, 1/2, 1/2, 1/2 }, layouts = 'communications' },
 
         { window = 'Youtube', rect = { 1/10, 0, 8/10, 1 }, layouts = 'media' },
     }
