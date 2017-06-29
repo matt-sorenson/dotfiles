1831fa470037f0ff6c5a1c8a563c6b92be31c8ab
Matt Sorenson <matt@mattsorenson.com>
2017-06-29 15:01:04 -0700

    fix issues with check_for_update on cloud desktop as result of using old zsh 5.0.7 (fix is in 5.0.8)

--- zsh/update.zsh
+++ zsh/update.zsh
@@ -1,30 +1,32 @@
 autoload -Uz colors && colors
 
+print_header(){
+    local color="$fg_bold[${1}]"
+    local header="================================================================================"
+    shift
+    local message="${@}"
+    echo "$color${header}\n= ${message}\n${header}$reset_color"
+}
+
 check_for_update() {
-    local print_header(){
-        local color="$fg_bold[${1}]"
-        local header="================================================================================"
-        shift
-        local message="${@}"
-        echo "$color${header}\n= ${message}\n${header}$reset_color"
-    }
-
-    local original_dir="$(pwd)"
-
-    local REPOS_TO_UPDATE=("${DOTFILES}" "${DOTFILES}/local" "${HOME}/.emacs.d" "${HOME}/.zprezto")
+    local -a REPOS_TO_UPDATE
+    REPOS_TO_UPDATE=( "${DOTFILES}" "${DOTFILES}/local" "${HOME}/.emacs.d" "${HOME}/.zprezto" )
     local OUT=0
 
     for dir in $REPOS_TO_UPDATE; do
         if [[ -d "${dir}" ]] && [[ -d "${dir}/.git" ]]; then
             print_header green "Updating ${dir}"
 
-            cd "${dir}"
+            pushd "${dir}" > /dev/null
 
             git fetch
-            local BRANCH=$(git status | grep -i "your branch is" | perl -pe "s|Your branch is behind '(.*?)' by \d* commit, and can be fast-forwarded.|\1|")
+            local BRANCH_STATUS="$(git status | grep -i "your branch is")"
+            if grep "and can be fast-forwarded" <<< "${BRANCH_STATUS}" &> /dev/null; then
+                local BRANCH="$(perl -pe "s|Your branch is behind '(.*?)' by \d* commit(s){0,1}, and can be fast-forwarded.|\1|" <<< "${BRANCH_STATUS}")"
 
-            if [[ -z "${BRANCH// }" ]]; then
-                git merge "$BRANCH"
+                if [[ -n "${BRANCH// }" ]]; then
+                    git merge "$BRANCH"
+                fi
             fi
 
             git status | grep "Your branch is up-to-date" > /dev/null
@@ -32,12 +34,12 @@ check_for_update() {
                 print_header red "Repo could not automaticly merge: ${dir}"
                 OUT=1
             fi
+
+            popd > /dev/null
         else
             print_header yellow "Missing Directory: ${dir}"
         fi
     done
-
-    cd "${original_dir}"
 
     return $OUT
 }
