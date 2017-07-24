c7032847e24bcb63fefee0fe17910f985f8199a4
Matt Sorenson <matt@mattsorenson.com>
2017-07-24 14:32:17 -0700

    add layout for CRs and remove adium from layout

--- hammerspoon/layouts/work-ultrawide-vertical.lua
+++ hammerspoon/layouts/work-ultrawide-vertical.lua
@@ -7,7 +7,7 @@ return {
         { app = {'Chrome', 'Firefox'},   rect = {2/3, 0, 1/3, 1} },
         { app = 'IntelliJ',              rect = {  0, 0, 2/3, 1} },
 
-        { window = 'CR%-.*Code Browser', rect = {  0, 0, 2/3, 1} },
+        { window = 'CR%-.*Code Browser', rect = {  0, 0, 2560/3440, 1} },
     }, { -- Secondary Screen
         screen = '1440x2560',
         { app = 'iTerm',                      rect = { 0,      0, 9/10,   5/9 } },
@@ -17,7 +17,6 @@ return {
         { window = 'Youtube',                 rect = {    0, 1/2,  3/4,   1/3 }, layouts = 'media' },
 
         { app = 'Messages',                   rect = {    0, 3/4,  1/2,   1/4 }, layouts = 'communications' },
-        { app = 'Adium',                      rect = {  1/3, 3/4,  1/3,   1/4 }, layouts = 'communications' },
         { app = 'Chime',                      rect = {  1/2, 3/4,  1/2,   1/4 }, layouts = 'communications' },
     },
 
