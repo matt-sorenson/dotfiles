4d3eb131c0635506851b0cc98e28e7bf11e2fb86
Matt Sorenson <matt@mattsorenson.com>
2018-02-19 17:06:15 -0800

    disable caffeinate menubar icon which doesn't work on high sierra

--- hammerspoon/ms/caffeine.lua
+++ hammerspoon/ms/caffeine.lua
@@ -37,11 +37,13 @@ local function timed_on_m(time_in_min)
     timed_on(hs.timer.minutes(time_in_min))
 end
 
+--[[
 if not menubar then
     menubar = hs.menubar.new(false)
     set(is_on())
     menubar:returnToMenuBar()
 end
+--]]
 
 add_cleanup_fn(function()
     if menubar then
@@ -50,32 +52,34 @@ add_cleanup_fn(function()
     end
 end)
 
-menubar:setMenu(function(keys)
-    local out = {
-        { title = ((is_on() and "Turn Off") or "Turn On"), fn = ((is_on() and off) or on) },
-        { title = '-' },
-        --{ title = '10 Seconds', fn = function() timed_on(10) end }, -- Uncomment this line for testing
-        { title = '30 Minutes', fn = function() timed_on_m(30) end },
-        { title = '60 Minutes', fn = function() timed_on_m(60) end }
-    }
+if menubar then
+    menubar:setMenu(function(keys)
+        local out = {
+            { title = ((is_on() and "Turn Off") or "Turn On"), fn = ((is_on() and off) or on) },
+            { title = '-' },
+            --{ title = '10 Seconds', fn = function() timed_on(10) end }, -- Uncomment this line for testing
+            { title = '30 Minutes', fn = function() timed_on_m(30) end },
+            { title = '60 Minutes', fn = function() timed_on_m(60) end }
+        }
 
-    if timer then
-        local time_left = timer:nextTrigger()
-        local fmt
+        if timer then
+            local time_left = timer:nextTrigger()
+            local fmt
 
-        if time_left < 60 then
-            fmt = '%is remaining'
-        else
-            fmt = '%im remaining'
-            time_left = math.floor(time_left / 60)
+            if time_left < 60 then
+                fmt = '%is remaining'
+            else
+                fmt = '%im remaining'
+                time_left = math.floor(time_left / 60)
+            end
+
+            table.insert(out, 1, { title = string.format(fmt, time_left) })
+            table.insert(out, 2, { title = '-' })
         end
 
-        table.insert(out, 1, { title = string.format(fmt, time_left) })
-        table.insert(out, 2, { title = '-' })
-    end
-
-    return out;
-end)
+        return out;
+    end)
+end
 
 return {
     set = set,
