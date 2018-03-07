79e0ad9ae383f21371efca1cf6bdbb7f00d05cbe
Matt Sorenson <matt@mattsorenson.com>
2018-03-07 15:51:17 -0800

    simplify some of the audio code and remove most of the is_work_computer differences

--- hammerspoon/ms/audio.lua
+++ hammerspoon/ms/audio.lua
@@ -1,34 +1,42 @@
 local sys = require 'ms.sys'
 
-local function setup_output()
-end
+local DEVICE_UIDS = {
+    monitor = 'AppleHDAEngineOutputDP:3,0,1,0:0:{AC10-A0A6-3034594C}',
+}
 
 if sys.is_work_computer() then
-    local DEVICE_UIDS = {
-        monitor = 'AppleHDAEngineOutputDP:3,0,1,0:0:{AC10-A0A6-3034594C}',
-        usb = 'AppleUSBAudioEngine:Unknown Manufacturer:C_Media USB Audio Device   :14120000:2,1'
-    }
+    DEVICE_UIDS['usb'] = 'AppleUSBAudioEngine:Unknown Manufacturer:C_Media USB Audio Device   :14120000:2,1'
+end
 
-    setup_output = function(device_name)
-        local requested_uid = DEVICE_UIDS[device_name]
+local function setup_output(device_name)
+    local requested_uid = DEVICE_UIDS[device_name] or device_name
 
-        if nil == requested_uid then return end
+    if nil == requested_uid then return end
 
-        local default_device_uid = hs.audiodevice.defaultOutputDevice():uid();
+    local default_device_uid = hs.audiodevice.defaultOutputDevice():uid();
 
-        if default_device_uid ~= requested_uid then
-            local work_device = hs.audiodevice.findDeviceByUID(requested_uid)
-            if work_device then
-                work_device:setDefaultOutputDevice()
-            end
+    if default_device_uid ~= requested_uid then
+        local work_device = hs.audiodevice.findDeviceByUID(requested_uid)
+        if work_device then
+            work_device:setDefaultOutputDevice()
         end
     end
 end
 
+local function get_device()
+    return hs.audiodevice.defaultOutputDevice()
+end
+
+local function get_volume()
+    return get_device():outputVolume()
+end
+
+local function set_volume(volume)
+    return get_device():setOutputVolume(volume)
+end
+
 local function update_volume(d_volume)
-    local default_out = hs.audiodevice.defaultOutputDevice()
-    local curr_volume = default_out:outputVolume()
-    default_out:setOutputVolume(math.max(0, math.min(100, curr_volume + d_volume)))
+    set_volume(math.max(0, math.min(100, get_volume + d_volume)))
 end
 
 local function update_volume_fn(d_volume)
@@ -36,6 +44,9 @@ local function update_volume_fn(d_volume)
 end
 
 return {
+    get_volume = get_volume,
+    set_volume = set_volume,
+
     update_volume_fn = update_volume_fn,
     update_volume = update_volume,
     setup_output = setup_output
