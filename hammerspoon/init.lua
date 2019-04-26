c0e2e1aa8bd7069a1d88570323203160aa26e60b
Matt Sorenson <matt@mattsorenson.com>
2019-04-25 21:37:22 -0700

    Moved hammerspoon helper functions to ms.helper

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,5 @@
 require 'hs.ipc'
+require 'ms.helper'
 
 hs.ipc.cliInstall()
 
@@ -7,46 +8,6 @@ hs.hotkey.bind({'ctrl', 'cmd'}, 'R', function()
     hs.notify.show('Hammerspoon', 'Reloading', '')
     hs.reload()
 end)
-
--- If the input is not a table then insert it as the first element of an array
--- and return that array. Usefull for functions that take 1 or more of a value
-function toarray(input)
-    if 'table' == type(input) then
-        return input
-    end
-
-    return {input}
-end
-
-table.keys = function(t)
-    local out = {}
-
-    for k, _ in pairs(t) do
-        table.insert(out, k)
-    end
-
-    return out
-end
-
-table.append = function(t1, ...)
-    for _, t2 in ipairs({...}) do
-        for _, v in ipairs(t2) do
-            table.insert(t1, v)
-        end
-    end
-
-    return t1
-end
-
-table.find = function(haystack, needle)
-    for k,v in pairs(haystack) do
-        if v == needle then
-            return k
-        end
-    end
-
-    return nil
-end
 
 -- use pcall to load the file so we can put the error in the console and
 -- continue to have the reload hotkey working
