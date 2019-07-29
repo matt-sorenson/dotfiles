ba924c64bfc6933149a8d079d4f088c2a4613d40
Matt Sorenson <matt@mattsorenson.com>
2019-07-28 17:44:14 -0700

    Updated hammerspoon layouts and fixed error message clearer

---
+++ hammerspoon/layouts/home-ultrawide.lua
@@ -0,0 +1,11 @@
+return {
+    name = "home-ultrawide",
+
+    { -- Primary Screen
+        screen = '3440x1440',
+
+        { app = {'Sublime', 'Quiver'}, rect = { 2/3, 0, 1/3,   1 } },
+        { app = {'Chrome', 'Firefox'}, rect = { 0,   0, 1/3,   1 } },
+        { app = 'iTerm',               rect = { 0,   0, 1/2, 1/2 } },
+w    }
+}
