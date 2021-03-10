1041532f1340412988be8337ea6f77d7e87b32c9
Matt Sorenson <matt@mattsorenson.com>
2021-03-10 09:45:17 -0800

    whitespace cleanup in keymap

--- hammerspoon/keymap.lua
+++ hammerspoon/keymap.lua
@@ -38,11 +38,11 @@ return {
             { key = 'Q', msg = 'Quiet current window',                 fn = layout.quiet_window_fn(1), optional_mods = 'shift' },
             { key = 'Q', mods = 'shift', msg = 'Quiet current window', fn = layout.quiet_window_fn(2), skip_help_msg = true },
 
-            { key = 'F', msg = 'Maximize',                    fn = layout.move_window_fn({   0,    0,   1,     1 })    },
-            { key = 'R', msg = 'Apply layout to window',      fn = layout.apply_to_window                              },
-            { key = 'T', msg = 'Apply Default Layout',        fn = layout.apply                                        },
-            { key = 'E', msg = 'Apply Media Layout',          fn = layout.apply_fn('Media')                            },
-            { key = 'C', msg = 'Apply Communications Layout', fn = layout.apply_fn('Communications')                   },
+            { key = 'F', msg = 'Maximize',                    fn = layout.move_window_fn({ 0, 0, 1, 1}) },
+            { key = 'R', msg = 'Apply layout to window',      fn = layout.apply_to_window               },
+            { key = 'T', msg = 'Apply Default Layout',        fn = layout.apply                         },
+            { key = 'E', msg = 'Apply Media Layout',          fn = layout.apply_fn('Media')             },
+            { key = 'C', msg = 'Apply Communications Layout', fn = layout.apply_fn('Communications')    },
 
             '-',
 
