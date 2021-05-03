c3f60e36759cecb29f8e23c6bdbfe3e361de5413
Matt Sorenson <matt@mattsorenson.com>
2021-05-03 10:13:07 -0700

    Move default slack position to deal with macos scaling issues

--- hammerspoon/layouts/work-ultrawide-vertical.lua
+++ hammerspoon/layouts/work-ultrawide-vertical.lua
@@ -11,12 +11,12 @@ return {
         screen = '1440x2560',
         { app = 'iTerm',                      rect = { 0,      0, 9/10,   5/9 } },
 
-        { window = {'reddit', 'Hacker News'}, rect = {  1/4, 5/9,  3/4, 11/28 }, layouts = 'media' },
-        { window = 'Youtube',                 rect = {    0, 1/2,    1,   1/2 }, layouts = 'media' },
-        { app = 'Music',                      rect = {    0, 1/2,    1,   1/2 }, layouts = 'media' },
+        { window = {'reddit', 'Hacker News'}, rect = { 1/4, 5/9,  3/4, 11/28 }, layouts = 'media' },
+        { window = 'Youtube',                 rect = {   0, 1/2,    1,   1/2 }, layouts = 'media' },
+        { app = 'Music',                      rect = {   0, 1/2,    1,   1/2 }, layouts = 'media' },
 
-        { window = 'Slack',                   rect = { 1/10,   0, 9/10,   1/2 }, layouts = 'communications' },
-        { app = 'Messages',                   rect = {    0, 2/3,  1/2,   1/3 }, layouts = 'communications' },
+        { window = 'Slack',                   rect = {   0,   0, 9/10,   1/2 }, layouts = 'communications' },
+        { app = 'Messages',                   rect = {   0, 2/3,  1/2,   1/3 }, layouts = 'communications' },
     },
 
     quiet_locations = {
