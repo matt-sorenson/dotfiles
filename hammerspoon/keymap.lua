62660f4f002eee316119abd2fef1df79f70ddcd0
Matt Sorenson <matt@mattsorenson.com>
2021-11-16 12:26:11 -0800

    Select Random PR Targets loads names from 'DOTFILES/local/teammates' instead of being hardcoded

--- hammerspoon/keymap.lua
+++ hammerspoon/keymap.lua
@@ -94,10 +94,19 @@ return {
         },
 
         { key = 'T', msg = 'Select Random PR Targets', fn = function()
-                local PR_TARGETS = {'Unspecified'}
-                local index = hs.math.random(1, #PR_TARGETS)
+                local TEAMMATE_FILENAME = hs.fs.pathToAbsolute("~/.dotfiles/local/teammates")
 
-                hs.alert(PR_TARGETS[index], 3)
+                if nil == hs.fs.attributes(TEAMMATE_FILENAME) then
+                    hs.alert("'" .. TEAMMATE_FILENAME .. "' is missing")
+                    return
+                end
+
+                local PR_TARGETS = {}
+                for name in io.lines(TEAMMATE_FILENAME)():gmatch('[^,%s]+') do
+                    PR_TARGETS[#PR_TARGETS + 1] = name
+                end
+
+                hs.alert(PR_TARGETS[hs.math.random(1, #PR_TARGETS)], 3)
             end
         },
     }
