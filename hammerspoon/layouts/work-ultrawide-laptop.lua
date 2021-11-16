7d6ddfc2f99e3d5d98bf0d2a0aeeab6bc79e1b72
Matt Sorenson <matt@mattsorenson.com>
2021-11-16 12:06:08 -0800

    Removed unused apps from layouts

--- hammerspoon/layouts/work-ultrawide-laptop.lua
+++ hammerspoon/layouts/work-ultrawide-laptop.lua
@@ -4,15 +4,12 @@ return {
     { -- Primary Screen
         screen = '3440x1440',
 
-        { app = 'Finder',              rect = {  0, 0, 1/6, 1} },
-        { app = {'Code', 'Quiver'},    rect = {1/6, 0, 1/2, 1} },
-        { app = {'Chrome', 'Firefox'}, rect = {2/3, 0, 1/3, 1} },
+        { app = 'Finder',                              rect = {  0, 0, 1/6, 1} },
+        { app = {'Code', 'DataGrip', 'IntelliJ IDEA'}, rect = {1/6, 0, 1/2, 1} },
+        { app = {'Chrome', 'Firefox'},                 rect = {2/3, 0, 1/3, 1} },
     }, { -- Secondary Screen
         screen = 'Color LCD',
 
-        { app = 'iTerm',                      rect = { 0,      0, 9/10,   5/9 } },
-
-        { window = {'reddit', 'Hacker News'}, rect = {  1/4, 5/9,  3/4, 11/28 }, layouts = 'media' },
         { window = 'Youtube',                 rect = {    0, .48,   .7,   1/3 }, layouts = 'media' },
         { window = 'Slack', rect = { 1/10,   0, 9/10, 9/10 }, layouts = 'communications' },
         { app = 'Messages', rect = { 1/10,   0, 9/10, 9/10 }, layouts = 'communications' },
--- hammerspoon/layouts/work-ultrawide-vertical.lua
+++ hammerspoon/layouts/work-ultrawide-vertical.lua
@@ -4,13 +4,12 @@ return {
     { -- Primary Screen
         screen = '3440x1440',
 
-        { app = 'Finder',                       rect = {  0, 0, 1/6, 1} },
-        { app = {'Code', 'Quiver', 'DataGrip', 'IntelliJ IDEA'}, rect = {  0, 0, 3/5, 1} },
-        { app = {'Chrome', 'Firefox'},          rect = {3/5, 0, 2/5, 1} },
-        { window = 'zoom',                      rect = {  3/5,   0,  2/5,     1 }, layouts = 'communications' },
+        { app = 'Finder',                              rect = {  0, 0, 1/6, 1} },
+        { app = {'Code', 'DataGrip', 'IntelliJ IDEA'}, rect = {  0, 0, 3/5, 1} },
+        { app = {'Chrome', 'Firefox'},                 rect = {3/5, 0, 2/5, 1} },
+        { window = 'zoom',                             rect = {3/5, 0, 2/5, 1 }, layouts = 'communications' },
     }, { -- Secondary Screen
         screen = '1440x2560',
-        { app = 'iTerm',                      rect = { 0,      0, 9/10,   5/9 } },
 
         { window = 'Youtube',                 rect = {   0, 1/2,    1,   1/2 }, layouts = 'media' },
         { app = 'Music',                      rect = {   0, 1/2,    1,   1/2 }, layouts = 'media' },
