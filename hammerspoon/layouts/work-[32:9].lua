54ff9e2933772e663c4172f5cb020a3586885613
Matt Sorenson <matt@mattsorenson.com>
2022-01-04 13:04:08 -0800

    modify slack position

--- hammerspoon/layouts/work-[32:9].lua
+++ hammerspoon/layouts/work-[32:9].lua
@@ -4,7 +4,7 @@ return {
             screen = {'5120x1440', 'LS49AG95'},
 
             { app = 'Messages', rect = { 1/6, 1/8, 1/6, 7/8 }, categories = 'communications' },
-            { window = 'Slack', rect = { 1/6,   0, 1/6, 7/8 }, categories = 'communications' },
+            { window = 'Slack', rect = { 1/9,  0, (1/6)+(1/18), 7/8 }, categories = 'communications' },
 
             { app = {'Code', 'DataGrip', 'IntelliJ IDEA'}, rect = {1/3, 0, 2/5, 1} },
             { app = {'Postman'}, rect = {0, 1/2-1/20, 1/3, 1/2 + 2/20} },
