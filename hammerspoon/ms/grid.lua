c6667073247a31bdc5933eb586fbefac9bde726c
Matt Sorenson <matt@mattsorenson.com>
2022-04-07 12:03:09 -0700

    cleanup grid setup

--- hammerspoon/ms/grid.lua
+++ hammerspoon/ms/grid.lua
@@ -10,34 +10,53 @@ local STANDARD_GRID = {
     {  'A',  'S',  'D',  'F',  'G',  'H' },
 }
 
-local ERGODOX_GRID = {
+-- While this looks really odd, for 49" ultrawide want 6x2 grid
+local MOONLANDER_GRID = {
     {  'Z',  'X',  'C',  'V',  'B',  'N' },
     { 'F1', 'F3', 'F4', 'F5', 'F6', 'F7' },
     {  '1',  '2',  '3',  '4',  '5',  '=' },
     {  'Q',  'W',  'E',  'R',  'T',  'HOME' },
     {  'A',  'S',  'D',  'F',  'G',  'H' },
-} --[[ {
-    { 'F1', 'F3', 'F4', 'F5', 'F6',   'F7' },
-    {  '1',  '2',  '3',  '4',  '5',    '6' },
-    {  'Q',  'W',  'E',  'R',  'T',    '=' },
-    {  'A',  'S',  'D',  'F',  'G',   'F2' },
-    {  'Z',  'X',  'C',  'V',  'B',  'END' }
 }
-]]--
 
 local function select_layout()
     if sys.find_usb_device_by_name('ErgoDox') or sys.find_usb_device_by_name('Moonlander Mark I') then
-        if ERGODOX_GRID ~= hs.grid.HINTS then
+        if MOONLANDER_GRID ~= hs.grid.HINTS then
             if debug_output.grid then
                 print('ergodox grid')
             end
-            hs.grid.HINTS = ERGODOX_GRID
+            hs.grid.HINTS = MOONLANDER_GRID
         end
     elseif STANDARD_GRID ~= hs.grid.HINTS then
         if debug_output.grid then
-            print('boring grid')
+            print('standard grid')
         end
         hs.grid.HINTS = STANDARD_GRID
+    end
+end
+
+local GRID_LAYOUTS = {
+    standard = {
+        ['3840x2160']   = '5x2',
+        ['DELL U3415W'] = '6x2',
+        ['3440x1440']   = '6x2',
+        ['5120x1440']   = '6x2',
+        ['LS49AG95']    = '6x2',
+        ['1440x2560']   = '3x4'
+    },
+    shift = {
+        ['3840x2160']   = '5x4',
+        ['DELL U3415W'] = '5x2',
+        ['3440x1440']   = '5x2',
+        ['5120x1440']   = '5x2',
+        ['LS49AG95']    = '5x2',
+        ['1440x2560']   = '4x4'
+    }
+}
+
+local function set_grid(layout)
+    for screen, dim in pairs(GRID_LAYOUTS[layout]) do
+        hs.grid.setGrid(dim, screen)
     end
 end
 
@@ -49,23 +68,7 @@ end
 
     hs.grid.setGrid('4x3', '2560×1440') -- 1440p Horizontal
 
-    if (mod == 'shift') then
-        hs.grid.setGrid('5x4', '3840x2160')   -- 4k Horizontal
-        hs.grid.setGrid('5x2', 'DELL U3415W') -- 34" Ultra-Wide
-        hs.grid.setGrid('5x2', '3440x1440')   -- 34" Ultra-Wide
-        hs.grid.setGrid('5x2', '5120x1440')   -- 49" Ultra-Wide
-        hs.grid.setGrid('5x2', 'LS49AG95')    -- 49" Ultra-Wide
-
-        hs.grid.setGrid('4x4', '1440x2560') -- 1440p Vertical
-    else
-        hs.grid.setGrid('5x2', '3840x2160')   -- 4k Horizontal
-        hs.grid.setGrid('6x2', 'DELL U3415W') -- 34" Ultra-Wide
-        hs.grid.setGrid('6x2', '3440x1440')   -- 34" Ultra-Wide
-        hs.grid.setGrid('6x2', '5120x1440')   -- 49" Ultra-Wide
-        hs.grid.setGrid('6x2', 'LS49AG95')    -- 49" Ultra-Wide
-
-        hs.grid.setGrid('3x4', '1440x2560') -- 1440p Vertical
-    end
+    set_grid(mod or 'standard')
 
     hs.grid.show()
 end
