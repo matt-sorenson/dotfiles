0796cf6bdb0a8086436ff5428130105db74e9d03
Matt Sorenson <matt@mattsorenson.com>
2022-09-16 12:41:08 -0700

    rename super squad to clinic services

--- hammerspoon/layouts/work-[32:9].lua
+++ hammerspoon/layouts/work-[32:9].lua
@@ -4,7 +4,7 @@ return {
             screen = {'5120x1440', 'LS49AG95'},
 
             { app = 'Messages', rect = { 1/6, 1/8, 1/6, 7/8 }, categories = 'communications' },
-            { window = 'Slack', rect = { 1/9,  0, (1/6)+(1/18), 7/8 }, categories = 'communications' },
+            { app = 'Slack', rect = { 1/9,  0, (1/6)+(1/18), 7/8 }, categories = 'communications' },
 
             { app = {'Postman'}, rect = {0, 1/2-1/20, 1/3, 1/2 + 3/40} },
 
