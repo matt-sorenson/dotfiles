bc0fee648a80631bfff18613ff7c497e66b14647
Matt Sorenson <matt@mattsorenson.com>
2023-10-26 15:13:31 -0700

    add layout for vscodium

--- hammerspoon/resources/layouts/home-[21:9].lua
+++ hammerspoon/resources/layouts/home-[21:9].lua
@@ -3,8 +3,8 @@ return {
         { -- Primary Screen
             screen = '3440x1440',
 
-            { app = 'Code',                rect = { 2/3, 0, 1/3,   1 } },
-            { app = {'Chrome', 'Firefox'}, rect = { 0,   0, 1/3,   1 } },
+            { app = { 'Code', 'VSCodium' },  rect = { 2/3, 0, 1/3,   1 } },
+            { app = { 'Chrome', 'Firefox' }, rect = { 0,   0, 1/3,   1 } },
         }
     },
 }
