3318918e0153bc29cbd3734cf02c9fd5721b3098
Matt Sorenson <matt@mattsorenson.com>
2023-10-26 15:13:31 -0700

    add layout for vscodium

--- hammerspoon/resources/layouts/home-[laptop].lua
+++ hammerspoon/resources/layouts/home-[laptop].lua
@@ -3,8 +3,8 @@ return {
         { -- Laptop Screen
             screen = 'Built-in Retina Display',
 
-            { app = 'Code',                rect = {   0, 0, 4/5, 1 } },
-            { app = {'Chrome', 'Firefox'}, rect = { 1/5, 0, 4/5, 1 } },
+            { app = { 'Code', 'VSCodium' },  rect = {   0, 0, 4/5, 1 } },
+            { app = { 'Chrome', 'Firefox' }, rect = { 1/5, 0, 4/5, 1 } },
 
             { window = 'Slack', rect = {   0, 0, 1/2, 1 }, layouts = 'communications' },
             { app = 'Messages', rect = { 1/2, 0, 1/2, 1 }, layouts = 'communications' },
--- hammerspoon/resources/layouts/work-[laptop].lua
+++ hammerspoon/resources/layouts/work-[laptop].lua
@@ -3,8 +3,8 @@ return {
         { -- Primary Screen
             screen = {'Retina', '64CAA675-3C76-B4FA-343A-76AEF8A35229'},
 
-            { app = {'Code', 'DataGrip', 'IntelliJ IDEA'}, rect = { 0,  0, 4/5, 1} },
-            { app = {'Chrome', 'Firefox'},                 rect = {1/5, 0, 4/5, 1} },
+            { app = {'Code', 'VSCodium', 'DataGrip', 'IntelliJ IDEA'}, rect = { 0,  0, 4/5, 1} },
+            { app = {'Chrome', 'Firefox'}, rect = {1/5, 0, 4/5, 1} },
 
             { window = 'Slack', rect = { 1/10,   0, 9/10, 9/10 }, categories = 'communications' },
             { app = 'Messages', rect = { 1/10,   0, 9/10, 9/10 }, categories = 'communications' },
