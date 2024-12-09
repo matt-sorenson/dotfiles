f04f5be068104f43b720cab27f4dc3aca281ca32
Matt Sorenson <matt@mattsorenson.com>
2024-12-09 11:12:16 -0800

    new monitor layout

---
+++ hammerspoon/resources/layouts/work-[9-16][16-9].lua
@@ -0,0 +1,29 @@
+return {
+    layout = {
+        { -- Primary Screen
+            screen = {'3840x1600'},
+
+            { app = {'Code', 'VSCodium', 'DataGrip', 'IntelliJ IDEA', 'TablePlus'}, section = 1, categories = 'primary' },
+
+            { app = {'Chrome'},  section = 2, categories = 'primary' },
+        },
+        {
+            screen = { 'DELL G2725D', '1440x2560' },
+
+            { app = {'Firefox', 'Music'}, section = 3, categories = 'media' },
+
+            { app = 'Messages', rect = { 7/8, 1/2, 7/8, 1/2 }, categories = 'communications' },
+            { app = 'Slack',    rect = {   0, 1/2, 7/8, 1/2 }, categories = 'communications' },
+        }
+    },
+
+    sections = {
+        { screen = 1, rect = {   0,   0, 5/8,   1 } },
+        { screen = 1, rect = { 5/8,   0, 3/8,   1 } },
+
+        { screen = 2, rect = {   0,   0,   1, 1/2 } },
+        { screen = 2, rect = {   0, 1/2,   1, 1/2 } },
+    },
+
+    is_work_computer = true,
+}
