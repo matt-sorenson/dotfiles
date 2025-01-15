1a7a04b4d85fc71c02cfe2a32731088ba6738c1d
Matt Sorenson <matt@mattsorenson.com>
2025-01-15 12:34:30 -0800

    format

--- hammerspoon/ms/icon.lua
+++ hammerspoon/ms/icon.lua
@@ -11,28 +11,28 @@ local BUTTON_HEIGHT = 96
 local shared_canvas = hs.canvas.new({ x = 0, y = 0, w = BUTTON_WIDTH, h = BUTTON_HEIGHT })
 
 local function get_icon_from_canvas(elements, options)
-  local width = options.width or BUTTON_WIDTH
-  local height = options.height or BUTTON_HEIGHT
+    local width = options.width or BUTTON_WIDTH
+    local height = options.height or BUTTON_HEIGHT
 
-  local canvas = shared_canvas
+    local canvas = shared_canvas
 
-  if width ~= BUTTON_WIDTH or height ~= BUTTON_HEIGHT then
-    canvas = hs.canvas.new({ x = 0, y = 0, w = width, h = height })
-  end
+    if width ~= BUTTON_WIDTH or height ~= BUTTON_HEIGHT then
+        canvas = hs.canvas.new({ x = 0, y = 0, w = width, h = height })
+    end
 
-  canvas:replaceElements(elements)
+    canvas:replaceElements(elements)
 
-  local image = canvas:imageFromCanvas()
+    local image = canvas:imageFromCanvas()
 
-  if canvas ~= shared_canvas then
-    canvas:delete()
-  end
+    if canvas ~= shared_canvas then
+        canvas:delete()
+    end
 
-  return image
+    return image
 end
 
 local function try_load_image(path)
-  return hs.image.imageFromPath(sys.get_resource_path('icons/' .. path))
+    return hs.image.imageFromPath(sys.get_resource_path('icons/' .. path))
 end
 
 local EXTENSIONS_TO_TRY = { '.png', '.svg' }
@@ -59,119 +59,119 @@ local EXTENSIONS_TO_TRY = { '.png', '.svg' }
 -- if this returns nil should probably `try_load_image('error.png')` to make
 -- it visually clear that the icon is missing
 local function load_icon_from_file(path)
-  local image = try_load_image(path)
-  if path then
-    return image
-  end
-
-  for _, ext in pairs(EXTENSIONS_TO_TRY) do
-    image = try_load_image(path .. ext)
-    if image then
-      return image
+    local image = try_load_image(path)
+    if path then
+        return image
     end
-  end
 
-  print("failed to load icon: " .. path)
+    for _, ext in pairs(EXTENSIONS_TO_TRY) do
+        image = try_load_image(path .. ext)
+        if image then
+            return image
+        end
+    end
+
+    print("failed to load icon: " .. path)
 end
 
 local function get_icon_from_file(path, options)
-  local image = try_load_image(path)
+    local image = try_load_image(path)
 
-  if not image then
-    image = try_load_image('error.png')
-  end
+    if not image then
+        image = try_load_image('error.png')
+    end
 
-  local elements = {}
+    local elements = {}
 
-  local width = options.width or BUTTON_WIDTH
-  local height = options.height or BUTTON_HEIGHT
+    local width = options.width or BUTTON_WIDTH
+    local height = options.height or BUTTON_HEIGHT
 
-  if not options.skip_background_color then
+    if not options.skip_background_color then
+        table.insert(elements, {
+            action = "fill",
+            frame = { x = 0, y = 0, w = width, h = height },
+            fillColor = options.background_color or colors.black,
+            type = "rectangle"
+        })
+    end
+
     table.insert(elements, {
-        action = "fill",
         frame = { x = 0, y = 0, w = width, h = height },
-        fillColor = options.background_color or colors.black,
-        type = "rectangle"
+        image = image,
+        type = "image",
+        transformation = options.transform,
     })
-  end
 
-  table.insert(elements, {
-      frame = { x = 0, y = 0, w = width, h = height },
-      image = image,
-      type = "image",
-      transformation = options.transform,
-  })
-
-  return get_icon_from_canvas(elements, options)
+    return get_icon_from_canvas(elements, options)
 end
 
 local function get_icon_from_text(text, options)
-  local options = options or { }
-  local text_color = options.text_color or colors.off_white
-  local font = options['font'] or ".AppleSystemUIFont"
-  local font_size = options['font_size'] or 70
-  local elements = {}
+    local options = options or { }
+    local text_color = options.text_color or colors.off_white
+    local font = options['font'] or ".AppleSystemUIFont"
+    local font_size = options['font_size'] or 70
+    local elements = {}
 
-  local width = options.width or BUTTON_WIDTH
-  local height = options.height or BUTTON_HEIGHT
+    local width = options.width or BUTTON_WIDTH
+    local height = options.height or BUTTON_HEIGHT
 
-  if not options.skip_background_color then
+    if not options.skip_background_color then
+        table.insert(elements, {
+            action = "fill",
+            frame = { x = 0, y = 0, w = width, h = height },
+            fillColor = options.background_color or colors.black,
+            type = "rectangle",
+        })
+    end
+
     table.insert(elements, {
-        action = "fill",
         frame = { x = 0, y = 0, w = width, h = height },
-        fillColor = options.background_color or colors.black,
-        type = "rectangle",
+        text = hs.styledtext.new(text, {
+            font = { name = font, size = font_size },
+            paragraphStyle = { alignment = "center" },
+            color = text_color,
+        }),
+        type = "text",
+        transformation = options.transform,
     })
-  end
 
-  table.insert(elements, {
-      frame = { x = 0, y = 0, w = width, h = height },
-      text = hs.styledtext.new(text, {
-          font = { name = font, size = font_size },
-          paragraphStyle = { alignment = "center" },
-          color = text_color,
-      }),
-      type = "text",
-      transformation = options.transform,
-  })
-
-  return get_icon_from_canvas(elements, options)
+    return get_icon_from_canvas(elements, options)
 end
 
 local function get_icon_for_color(color, options)
-  local width = options.width or BUTTON_WIDTH
-  local height = options.height or BUTTON_HEIGHT
+    local width = options.width or BUTTON_WIDTH
+    local height = options.height or BUTTON_HEIGHT
 
-  local elements = {
-    {
-      action = "fill",
-      frame = { x = 0, y = 0, w = width, h = height },
-      fillColor = color or colors.black,
-      type = "rectangle",
+    local elements = {
+        {
+            action = "fill",
+            frame = { x = 0, y = 0, w = width, h = height },
+            fillColor = color or colors.black,
+            type = "rectangle",
+        }
     }
-  }
 
-  return get_icon_from_canvas(elements, options)
+    return get_icon_from_canvas(elements, options)
 end
 
 --[[ export ]] local function get_icon(options)
-  if type(options) == 'string' then
-    options = { text = options }
-  end
+    if type(options) == 'string' then
+        options = { text = options }
+    end
 
-  if options.path then
-    return get_icon_from_file(options.path, options)
-  elseif options.text then
-    return get_icon_from_text(options.text, options)
-  elseif options.color then
-    return get_icon_for_color(options.color, options)
-  elseif options.canvas then
-    return get_icon_from_canvas(options.canvas, options)
-  else
-    print('You must provide either a path or text to get_icon')
-  end
+    if options.path then
+        return get_icon_from_file(options.path, options)
+    elseif options.text then
+        return get_icon_from_text(options.text, options)
+    elseif options.color then
+        return get_icon_for_color(options.color, options)
+    elseif options.canvas then
+        return get_icon_from_canvas(options.canvas, options)
+    else
+        print('You must provide either a path or text to get_icon')
+    end
 end
 
 return {
-  get_icon = get_icon
+    get_icon = get_icon
 }
