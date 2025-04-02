1f562f73caed5cc087aa737e39c08524522bca42
Matt Sorenson <matt@mattsorenson.com>
2025-04-02 15:19:32 -0700

    move ms.helper

--- hammerspoon/ms/helper/table.lua
+++ hammerspoon/ms/helper/table.lua
@@ -1,18 +1,3 @@
--- I end up looking up how to clear the console each time cause I forget about
--- `Console` on the end of the method name.
-hs.console.clear = hs.console.clearConsole
-
---- If the input is not a table then insert it as the first element of an array
--- and return that array. Useful for functions whose input can be a singular
--- value or an array of values.
-function toarray(input)
-    if 'table' == type(input) then
-        return input
-    end
-
-    return { input }
-end
-
 --- this function returns a list of all the keys in a table
 table.keys = function(t)
     local out = {}
