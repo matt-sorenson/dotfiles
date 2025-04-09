ea330f60d33e6b3e0918a5b367de34d7ada1c4ae
Matt Sorenson <matt@mattsorenson.com>
2025-04-09 15:16:56 -0700

    add color fallback for failing to load icon

--- hammerspoon/ms/icon/from_file.lua
+++ hammerspoon/ms/icon/from_file.lua
@@ -2,17 +2,26 @@ local print = require('ms.logger').new('ms.icon')
 
 local colors = require('ms.colors').streamdeck
 local fs = require 'ms.fs'
+local from_color = require('ms.icon.from_color')
 
 local function try_load_image(path)
     return hs.image.imageFromPath(fs.get_resource_path('icons/' .. path))
 end
 
+--[[ export ]]
 local function get_canvas_from_file(path, options)
     local image = try_load_image(path)
 
+    -- If the image didn't load use the error fallback image.
     if not image then
         print:warnf("Failed to load image from path '%s'", path)
         image = try_load_image('error.png')
+    end
+
+    -- If the image is still not loaded, fallback to an annoying color.
+    if not image then
+        print:error("Failed to load error image")
+        return from_color(colors.missing, options)
     end
 
     local elements = {}
