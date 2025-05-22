653fa9eef96e26482412f3de592f02ed7a4369fb
Matt Sorenson <matt@mattsorenson.com>
2025-05-22 11:50:35 -0700

    lazy initialize nvm for much, much faster shell startup times

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -39,9 +39,34 @@ if [ -f "${HOME}/.cargo/env" ]; then
     source "${HOME}/.cargo/env"
 fi
 
+# Lazy load nvm to speed up shell startup time, though the first call to
+# npm/node/nvm/pnpm will be slower.
 if [ -d "${HOME}/.nvm" ]; then
-    export NVM_DIR="${HOME}/.nvm"
-    [ -s "${NVM_DIR}/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  function _lazy_load_nvm() {
+    unset -f npm node nvm pnpm
+    export NVM_DIR=~/.nvm
+    [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+  }
+
+  function npm() {
+    _lazy_load_nvm
+    npm $@
+  }
+
+  function node() {
+    _lazy_load_nvm
+    node $@
+  }
+
+  function nvm() {
+    _lazy_load_nvm
+    nvm $@
+  }
+
+  function pnpm() {
+    _lazy_load_nvm
+    pnpm $@
+  }
 fi
 
 if type rbenv > /dev/null ; then
