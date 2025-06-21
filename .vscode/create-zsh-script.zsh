5f215a1a915fd44b75dee610b3222a0d4e051a81
Matt Sorenson <matt@mattsorenson.com>
2025-06-21 15:24:40 -0700

    make vscode task create ZSH script only focus it's output if there's an error

--- .vscode/create-zsh-script.zsh
+++ .vscode/create-zsh-script.zsh
@@ -1,28 +1,34 @@
 #!/usr/bin/env zsh
 
+emulate -L zsh
+set -euo pipefail
+setopt typeset_to_unset
+
+print-header "Starting create-zsh-script.zsh"
+
 if (( $# == 0 )) || [[ -z "$1" ]]; then
   print-header -e "Script name not provided." >&2
-  exit 1
+  return 1
 fi
 
 if (( $# > 1 )); then
   print-header -e "To many arguments provided \`$*\`." >&2
-  exit 1
+  return 1
 fi
 
 name="$1"
 
 template="${DOTFILES}/templates/bin/new_script.zsh"
-target="${DOTFILES}/bin/${name}.zsh"
+target="${DOTFILES}/bin/${name}"
 
 if [[ ! -r "$template" ]]; then
   print-header -e "Template '$template' not found." >&2
-  exit 1
+  return 1
 fi
 
 if [[ -e "$target" ]]; then
   print-header -e "File '$target' already exists." >&2
-  exit 1
+  return 1
 fi
 
 mkdir -p bin
