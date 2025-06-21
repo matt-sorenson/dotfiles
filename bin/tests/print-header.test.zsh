6410812333a4405304b27db3aacc980cb8406e25
Matt Sorenson <matt@mattsorenson.com>
2025-06-21 14:16:48 -0700

    remove print-header- from filenames in expected-results/print-header/

--- bin/tests/print-header.test.zsh
+++ bin/tests/print-header.test.zsh
@@ -4,34 +4,34 @@ source "${DOTFILES}/bin/tests/harness.zsh"
 
 # For the tests here since they deal with the colors directly leave the color codes in the expected_results files
 
-function print-header-happycase() {
+function happycase() {
     print-header 'Test'
 }
 
-function print-header-happycase-indent() {
+function happycase-indent() {
     print-header --indent 2 'Test'
 }
 
-function print-header-happycase-icon() {
+function happycase-icon() {
     print-header --icon 🌖 'Test'
 }
 
-function print-header-happycase-warning-icon() {
+function happycase-warning-icon() {
     # This can end up looking odd in a text editor
     # the space after the moon in the output is added for visual alignment
     # in the terminal itself.
     print-header -w --icon 🌖 'Test'
 }
 
-function print-header-happycase-warning() {
+function happycase-warning() {
     print-header -w 'Test'
 }
 
-function print-header-happycase-error() {
+function happycase-error() {
     print-header -e 'Test'
 }
 
-function print-header-flag-after-double-dash() {
+function flag-after-double-dash() {
     # header should say `--icon 🌖 --indent 2 -e Test` as it's message.
     print-header -- --icon 🌖 --indent 2 -e 'Test'
 }
@@ -40,13 +40,13 @@ function main() {
     local out=0
     local testee='print-header'
     local -a test_cases=(
-        'print-header-happycase'
-        'print-header-happycase-indent'
-        'print-header-happycase-icon'
-        'print-header-happycase-warning-icon'
-        'print-header-happycase-warning'
-        'print-header-happycase-error'
-        'print-header-flag-after-double-dash'
+        'happycase'
+        'happycase-indent'
+        'happycase-icon'
+        'happycase-warning-icon'
+        'happycase-warning'
+        'happycase-error'
+        'flag-after-double-dash'
     )
 
     for element in "${(k)test_cases[@]}"; do
