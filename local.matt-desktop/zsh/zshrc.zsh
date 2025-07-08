c04753b135d857a1f48e712d548d441998b60ecd
Matt Sorenson <matt@mattsorenson.com>
2025-07-07 17:05:39 -0700

    fix wsl-clone autocomplete

--- local.matt-desktop/zsh/zshrc.zsh
+++ local.matt-desktop/zsh/zshrc.zsh
@@ -2,5 +2,5 @@ wsl-clone() {
     ws-clone --cmd-name "wsl-clone" --root /mnt/d/ws --soft-link "${WORKSPACE_ROOT_DIR}" "$@"
 }
 
-# Because [_ws-clone] has the '-' in it we use this strange syntax...
-typeset 'dotfiles_completion_functions[_ws-clone]'='wsl-clone'
+# Because [wsl-clone] has the '-' in it we use this strange syntax...
+typeset 'dotfiles_completion_functions[wsl-clone]'='_ws-clone'
